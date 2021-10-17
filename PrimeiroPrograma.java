b02d83829a62638c1fcf567984b57c1598f82697
Ericky2022 <erivaldo.pereira.santana2016@yahoo.com.br>
2021-10-17 12:27:34 -0300

    First commit

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package brcom.dio;
+
+import brcom.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Primeiro Programa " + (a+b));*/
+	}
+
+}
